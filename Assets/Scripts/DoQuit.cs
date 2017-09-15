f48f59ab58fddb810e07d578d30c52d51f1703d5
Angelo Orgas <orgas002@cougars.csusm.edu>
2017-09-14 23:14:44 -0700

    Initial Commit, Homework # 1 CSUSM CS 485

---
+++ Assets/Scripts/DoQuit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoQuit : MonoBehaviour {
+
+	public void ExecuteQuit()
+	{
+		Debug.Log("Has Quit Game");
+		Application.Quit();
+	}
+}
